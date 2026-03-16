39c1a46f2b5a80615073937d2a0301e55ffd4644
Carla Talita <caarlatalita1@hotmail.com>
2026-03-16 01:47:25 -0300

    feat: Adding README and fixing controllers

--- src/main/java/com/betrybe/alexandria/controller/AuthorController.java
+++ src/main/java/com/betrybe/alexandria/controller/AuthorController.java
@@ -7,9 +7,11 @@ import com.betrybe.alexandria.service.AuthorService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -43,11 +45,13 @@ public class AuthorController {
 
   }
 
+  @PutMapping("/{id}")
   public AuthorDto updateAuthor(@PathVariable Long id,
       @RequestBody AuthorCreationDto authorCreationDto) {
     return AuthorDto.fromEntity(authorService.update(id, authorCreationDto.toEntity()));
   }
 
+  @DeleteMapping("/{id}")
   public AuthorDto deleteAuthorById(@PathVariable Long id) {
     return AuthorDto.fromEntity(authorService.deleteById(id));
   }
--- src/main/java/com/betrybe/alexandria/controller/PublisherController.java
+++ src/main/java/com/betrybe/alexandria/controller/PublisherController.java
@@ -1,5 +1,58 @@
 package com.betrybe.alexandria.controller;
 
+import com.betrybe.alexandria.controller.dto.PublisherCreationDto;
+import com.betrybe.alexandria.controller.dto.PublisherDto;
+import com.betrybe.alexandria.service.PublisherService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publishers")
 public class PublisherController {
+
+  private final PublisherService publisherService;
+
+  @Autowired
+  public PublisherController(PublisherService publisherService) {
+    this.publisherService = publisherService;
+  }
+
+  @GetMapping("/{id}")
+  public PublisherDto getPublisherById(@PathVariable Long id) {
+    return PublisherDto.fromEntity(publisherService.findById(id));
+  }
+
+  @GetMapping
+  public List<PublisherDto> getAllPublishers() {
+    return publisherService.findAll().stream().map(PublisherDto::fromEntity).toList();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public PublisherDto createPublisher(@RequestBody PublisherCreationDto publisherCreationDto) {
+    return PublisherDto.fromEntity(publisherService.create(publisherCreationDto.toEntity()));
+  }
+
+  @PutMapping({"/id"})
+  public PublisherDto updatePublisher(@PathVariable Long id,
+      @RequestBody PublisherCreationDto publisherCreationDto) {
+    return PublisherDto.fromEntity(publisherService.update(id, publisherCreationDto.toEntity()));
+  }
+
+  @DeleteMapping("/{id}")
+  public PublisherDto deletePublisherById(@PathVariable Long id) {
+    return PublisherDto.fromEntity((publisherService.deleteById(id)));
+
+  }
 
 }
